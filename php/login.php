67c6f842cb318bdeed2bd9c756309617a91fc335
Priyadarshini Saha <2002sahapriya@gmail.com>
2021-09-18 16:06:57 -0400

    Login page

--- php/login.php
+++ php/login.php
@@ -9,11 +9,31 @@
 
     
     if($_SERVER["REQUEST_METHOD"] == "POST") {
+        // username validation
         if(empty($_POST["username"])) {
             $username_err = "Please enter a username";
         } else {
             $username = validate_data($_POST["username"]);
         }
+        // email validation 
+        if(empty($_POST["email"])) {
+            $email_err = "Please enter an email";
+        } else {
+            $email = validate_data($_POST["email"]);
+        }
+
+        // password validation 
+        if(empty($_POST["password"])) {
+            $password_err = "Please enter a password";
+        } else {
+            $password = validate_data($_POST["password"]);
+        }
+
+        echo $username;
+        echo $password;
+        echo $email;
+
+
     }
 
     function validate_data($data) {
@@ -23,6 +43,7 @@
         return $data;
     }
 
+    
 
 ?>
 
