bd0b373c2c979af87baac6c7c5713ab1a4e4f955
Priyadarshini Saha <2002sahapriya@gmail.com>
2021-09-17 23:23:22 -0400

    The main file structure
    Add images to assets
    Add javascript files to javascript directory
    Add HTML files to View
    Add php files to PHP
    Add Css files to css

---
+++ php/landing.php
@@ -0,0 +1,16 @@
+<?php
+// This for landing page
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        
+<head>
+    <title>Landing Page</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    </head>
+    <body></body>
+</html>
---
+++ php/login.php
@@ -0,0 +1,30 @@
+<?php
+// This for login page 
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h1>Login to Toshi</h1>
+    <h3>This is or that ?</h3>
+    <!-- Login form-->
+    <form>
+        <!--Username-->
+        <label for="username"> Username:</label><br>
+        <input type="text" name="username" id="username" placeholder="Enter Username"> <br>
+        <!--Email-->
+        <label for="email">Email:</label> <br>
+        <input type="text" name="email" id="email" placeholder="Enter Email"><br>
+        <!--Password-->
+        <label for="password">Password:</label><br>
+        <input type="password" name="password" id="password" placeholder="Enter Password"><br>
+    </form>
+</body>
+</html>
